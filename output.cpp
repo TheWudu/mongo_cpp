b1c38b4db708b633e4350bb44d18b96be4ba7b90
TheWudu <mfuehrlinger@gmx.at>
2021-07-25 12:52:20 +0200

    Improve session list output

--- output.cpp
+++ output.cpp
@@ -10,13 +10,22 @@ void Output::print_session_list(std::vector<Models::Session> sessions) {
   using namespace std;
     
   uint32_t notes_len = 75;
+  
+  size_t max_size_dur = 0;
+  size_t max_size_dis = 0;
+  size_t max_size_typ = 0;
+  for(const auto session : sessions) {
+    max_size_typ = std::max(max_size_typ, Helper::SportType::name(session.sport_type_id).length()); 
+    max_size_dur = std::max(max_size_dur, Helper::TimeConverter::ms_to_min_str(session.duration).length());
+    max_size_dis = std::max(max_size_dis, std::to_string(session.duration).length());
+  }
 
   for(auto session: sessions) {
     cout <<       setw(38) << session.id
       << " - " << setw(17) << Helper::TimeConverter::time_to_string(session.start_time)
-      << " - " << setw(18) << left << Helper::SportType::name(session.sport_type_id)
-      << " - " << setw(5)  << right << (session.distance > 0 ? std::to_string(session.distance) : "") 
-      << " - " << setw(7)  << right << Helper::TimeConverter::ms_to_min_str(session.duration)
+      << " - " << setw(max_size_typ) << left << Helper::SportType::name(session.sport_type_id)
+      << " - " << setw(max_size_dis) << right << (session.distance > 0 ? std::to_string(session.distance) : "") 
+      << " - " << setw(max_size_dur) << right << Helper::TimeConverter::ms_to_min_str(session.duration)
       << " - " << session.notes.substr(0, notes_len) << (session.notes.size() > notes_len ? "..." : "")
       << endl;
   }
