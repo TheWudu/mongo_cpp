339903c552674f60c61b724d4ded2eee427aa30f
TheWudu <mfuehrlinger@gmx.at>
2021-07-27 06:21:18 +0200

    Print time properly

--- helper/time_converter.cpp
+++ helper/time_converter.cpp
@@ -8,7 +8,7 @@
 std::string Helper::TimeConverter::time_to_string(const time_t time) {
   char buf[19];
   struct tm * timeinfo = localtime(&time);
-  strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%MZ", timeinfo);
+  strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M", timeinfo);
 
   return std::string { buf };
 }
@@ -22,7 +22,6 @@ time_t Helper::TimeConverter::date_time_string_to_time_t(const std::string time_
 
 
 time_t Helper::TimeConverter::string_to_time_t( const std::string time_str ) {
-
   boost::gregorian::date date = boost::gregorian::from_simple_string(time_str);
   static boost::posix_time::ptime epoch(boost::gregorian::date(1970, 1, 1));
   boost::posix_time::time_duration::sec_type secs = (boost::posix_time::ptime(date,boost::posix_time::seconds(0)) - epoch).total_seconds();
